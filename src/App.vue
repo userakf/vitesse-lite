62df544ed0150e8d02edf1869948d6391549f47b
userakf <820279897@qq.com>
2022-09-15 00:27:22 +0800

    ui:update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-    <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-        <router-view />
-        <Footer />
+    <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200" flex place-items-center>
+        <div w-full>
+            <router-view />
+            <Footer />
+        </div>
     </main>
 </template>
